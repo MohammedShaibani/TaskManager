678620fc9f5479fea9d486b75d145bb7bf937656
MohammedShaibani <sonngohun@hotmail.ca>
2021-10-12 19:29:04 -0400

    Added task form and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [task, setTask] = useState('');
+  const [assignedTo, setAssignedTo] = useState('');
+
+
   return (
     <div className="App">
+      <div className="information">
+        <label>Task</label>
+        <input type="text" onChange={(event) => (setTask(event.target.value))} />
+        <label>Assigned To</label>
+        <input type="text" onChange={(event) => (setAssignedTo(event.target.value))}/>
+        <button>Add Task</button>
+      </div>
     </div>
   );
 }
